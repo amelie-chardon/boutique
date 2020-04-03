af9a75c938a0ebbfc6cdc09b13b5f7eaa2b91e06
sarah <sarah.chaouati@laplateforme.io>
2020-04-03 08:55:24 +0200

    merge amelie 02/04

--- include/header.php
+++ include/header.php
@@ -7,15 +7,14 @@
         <img id="logo-top" src="img/logo.png">
         <h1>Amélie & Sarah</h1>
     </div>
-    <div id="search">
 
-    </div>
     <div id="menu2">
         <?php require 'include/nav2.php'?>
-       
-        <form method="GET" action="resultat_recherche.php">
-            <input type="text" name="search" placeholder="Search">
-            <input type="submit" value="Chercher">
-        </form>
+        <div id="search">
+            <form method="GET" action="resultat_recherche.php">
+                <input type="text" name="search" placeholder="Search">
+                <input type="submit" value="Chercher">
+            </form>
+        </div>
     </div>
 </header>
--- include/nav2.php
+++ include/nav2.php
@@ -1,8 +1,24 @@
+<?php 
+
+$_SESSION["bdd"]->connect();
+$categories=$_SESSION["bdd"]->execute("SELECT nom FROM categories");
+
+?>
+
 <nav>
     <ul>
         <li><a href="index.php">Accueil</a></li>
-        <li><a href="index.php">Nos produits</a></li>
+        <li class="deroulant"><a href="#">Nos produits</a>
+            <ul class="sous">
+        <?php foreach ($categories as $nom_cat)
+        {
+            ?><li><a href="resultat_recherche.php?search=<?php echo $nom_cat[0]; ?>"><?php echo $nom_cat[0]; ?></a></li><?php
+        }
+        ?>
+            </ul>
+        </li>
         <li><a href="presentation.php">Notre entreprise</a></li>
         <li><a href="contact.php">Contact</a></li> 
      </ul>
 </nav>
+
